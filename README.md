76e3c0aa8004f8dc45d6f93d9f2432a91dfe39ea
yiss02 <mathis.lancey@edu.esiee.fr>
2024-12-24 15:11:26 +0100

    Add main.java

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# testing add new repository
+# Sommes des plus petites valeurs positives des lignes de T.
+
+Exemple :
+
+tableau T : 
+[-1, 0, 2, 3]
+[-5, -3, -2, -1]
+[-1, 1, 3, 4]
+
+Somme des plus petites valeurs positives de ses lignes : 3
--- main.java
+++ main.java
@@ -1,2 +1,40 @@
-print("Hello World");
+import java.util.Arrays;
+class main{ // Sommes des plus petites valeurs positives des lignes de T.
+		public static void main(String[] args){
+			int[][] T = {{-1,0,2,3}, {-5,-3,-2,-1}, {-1,1,3,4}};
+			System.out.println("tableau T : ");
+			afficher(T);
+			System.out.printf("Somme des plus petites valeurs positives de ses lignes : %d\n",
+				sppvpl(T)
+			);
+		}
+		static int sppvpl(int[][] T){ int m = T.length, n = T[0].length;
+			int s = 0; // somme des valeurs plus petites val. positives des lignes de T
+			for(int i=0; i<m; i++){
+                int k=0;
+                for(int j=0; j<n; j++){
+                    if(T[i][j]<=0)k++;
+                }
+                if(k<n){
+					s += T[i][k];
+				}
+            }
+			return s; 		
+		}
+		// Fonction annexe : afficher T
+		static void afficher(int[][] T){ int m = T.length;
+			for (int i = 0; i < m; i++)
+				System.out.println(Arrays.toString(T[i]));
+		}	
+}
+/* compilation et exécution dans un terminal Unix.
+% javac Exercice2.java
+% java Exercice2
+tableau T : 
+[-1, 0, 2, 3]
+[-5, -3, -2, -1]
+[-1, 1, 3, 4]
+Somme des plus petites valeurs positives de ses lignes : 3
+% 
 
+*/
